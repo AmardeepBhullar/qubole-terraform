6f1170ca258a0b6a7ac78174dd0526ddf0c2ed4c
AmardeepBhullar <47836619+AmardeepBhullar@users.noreply.github.com>
2023-09-24 19:38:58 -0700

    Update whatsmyip.sh

--- qubole-aws-terraform-deployment/qubole-deployment/modules/hive_metastore/scripts/shell/whatsmyip.sh
+++ qubole-aws-terraform-deployment/qubole-deployment/modules/hive_metastore/scripts/shell/whatsmyip.sh
@@ -3,5 +3,5 @@
 #credits https://www.itwonderlab.com/use-your-public-internet-ip-address-terraform/
 
 set -e
-INTERNETIP="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+INTERNETIP="$(dig +short myip.opendns.com @resolver1.opendns.com -4)"
 jq -n --arg internetip "$INTERNETIP" '{"internet_ip":$internetip}'
